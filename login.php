805504c6a879802a70ee61f37e054f909bec4b9f
Jeric James Viernes <jericviernes06@gmail.com>
2024-03-21 10:36:28 +0800

    login page

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="src/output.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="w-full h-screen rounded-3xl relative">
+        <div class="w-full flex items-center justify-between p-4">
+            <a href="index.php" class="w-1/3 text-2xl font-bold text-[#1c1c1c]">PAW'FECT MATCH</a>
+            <div class="w-1/3 flex justify-end gap-2">
+                <a href="#" class="px-6 py-1 rounded-full bg-[#e9e9e9]">Signup</a>
+                <button>Login</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
