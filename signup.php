09177d0dd91ef27336759c47e009edd7230f8d99
Jeric James Viernes <jericviernes06@gmail.com>
2024-03-22 13:16:36 +0800

    code clean up

--- signup.php
+++ signup.php
@@ -11,7 +11,7 @@
     <title>Document</title>
 </head>
 <body class="bg-[#cdcdcd] text-sm overflow-hidden h-screen relative">
-    <div class="w-3/4 h-[80%] shadow-xl bg-[#ffc8a8] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl">
+    <div class="w-3/4 h-[80%] shadow-xl bg-[#ffc8a8] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl z-0">
         <div class="w-full h-full flex items-center justify-center">
             <div class="w-2/5 relative">
                 <div>
@@ -29,48 +29,42 @@
                 <p class="text-2xl">Welcome to <span class="font-semibold">Pawfect!</span></p>
                 <p class="text-slate-800 mb-10">Create your first account.</p>
                 <form action="Controller/UserController.php" method="POST" class="w-full">
+                    <div id="form" class="hidden w-1/3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-500 z-10 p-4">
+                        <p class="text-xl font-bold text-center">VERIFY YOUR EMAIL ADDRESS</p>
+                        <p class="text-sm text-center font-medium">A code has been sent to your email address</p>
+                        <button name="continue" class="bg-[#ffc8a8] px-10 py-2 rounded-xl">Continue</button>
+                    </div>
                     <div class="w-full flex gap-2 mb-5">
                         <div class="w-1/2 flex flex-col gap-1">
                             <label for="">First name</label>
-                            <input type="text" name="first_name" class="w-full p-2 outline-none rounded-xl border border-[#ffc8a8]">
+                            <input type="text" name="first_name" class="w-full p-2 outline-none rounded-xl border focus:border-[#ffc8a8] shadow-md">
                         </div>
                         <div class="w-1/2 flex flex-col gap-1">
                             <label for="">Last name</label>
-                            <input type="text" name="last_name" class="w-full p-2 outline-none rounded-xl border border-[#ffc8a8]">
+                            <input type="text" name="last_name" class="w-full p-2 outline-none rounded-xl border focus:border-[#ffc8a8] shadow-md">
                         </div>
                     </div>
                     <div class="w-full flex gap-2 mb-5">
                         <div class="w-1/2 flex flex-col gap-1">
                             <label for="">Email Address</label>
-                            <input type="email" name="email" class="w-full p-2 outline-none rounded-xl border border-[#ffc8a8]">
+                            <input type="email" name="email" class="w-full p-2 outline-none rounded-xl border focus:border-[#ffc8a8] shadow-md">
                         </div>
                         <div class="w-1/2 flex flex-col gap-1">
                             <label for="">Contact Number</label>
-                            <input type="text" name="contact" class="w-full p-2 outline-none rounded-xl border border-[#ffc8a8]">
+                            <input type="text" name="contact" class="w-full p-2 outline-none rounded-xl border focus:border-[#ffc8a8] shadow-md">
                         </div>
                     </div>
                     <div class="w-full mb-5">
                         <label for="">Present Address</label>
-                        <input type="text" name="address" class="w-full p-2 outline-none rounded-xl border border-[#ffc8a8]">
+                        <input type="text" name="address" class="w-full p-2 outline-none rounded-xl border focus:border-[#ffc8a8] shadow-md">
                     </div>
                     <div class="w-full flex gap-2 mb-8">
                         <div class="w-1/2 flex flex-col gap-1">
                             <div>
                                 <label for="">Password</label>
                                 <div class="relative">
-                                    <input id="pass" type="password" name="password" class="w-full p-2 outline-none rounded-xl border border-[#ffc8a8]">
+                                    <input id="pass" type="password" name="password" class="w-full p-2 outline-none rounded-xl border focus:border-[#ffc8a8] shadow-md">
                                     <button type="submit" onclick="togglePassword()" class="absolute right-2 top-1/2 transform -translate-y-1/2" >
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" height="15" width="15" fill="#4b4b4b"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M288 80c-65.2 0-118.8 29.6-159.9 67.7C89.6 183.5 63 226 49.4 256c13.6 30 40.2 72.5 78.6 108.3C169.2 402.4 222.8 432 288 432s118.8-29.6 159.9-67.7C486.4 328.5 513 286 526.6 256c-13.6-30-40.2-72.5-78.6-108.3C406.8 109.6 353.2 80 288 80zM95.4 112.6C142.5 68.8 207.2 32 288 32s145.5 36.8 192.6 80.6c46.8 43.5 78.1 95.4 93 131.1c3.3 7.9 3.3 16.7 0 24.6c-14.9 35.7-46.2 87.7-93 131.1C433.5 443.2 368.8 480 288 480s-145.5-36.8-192.6-80.6C48.6 356 17.3 304 2.5 268.3c-3.3-7.9-3.3-16.7 0-24.6C17.3 208 48.6 156 95.4 112.6zM288 336c44.2 0 80-35.8 80-80s-35.8-80-80-80c-.7 0-1.3 0-2 0c1.3 5.1 2 10.5 2 16c0 35.3-28.7 64-64 64c-5.5 0-10.9-.7-16-2c0 .7 0 1.3 0 2c0 44.2 35.8 80 80 80zm0-208a128 128 0 1 1 0 256 128 128 0 1 1 0-256z"/></svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="w-1/2 flex flex-col gap-1">
-                            <div>
-                                <label for="">Repeat Password</label>
-                                <div class="relative">
-                                    <input id="rpass" type="password" name="rpassword" class="w-full p-2 outline-none rounded-xl border border-[#ffc8a8]">
-                                    <button type="submit" onclick="toggleRPassword()" class="absolute right-2 top-1/2 transform -translate-y-1/2" >
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" height="15" width="15" fill="#4b4b4b"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M288 80c-65.2 0-118.8 29.6-159.9 67.7C89.6 183.5 63 226 49.4 256c13.6 30 40.2 72.5 78.6 108.3C169.2 402.4 222.8 432 288 432s118.8-29.6 159.9-67.7C486.4 328.5 513 286 526.6 256c-13.6-30-40.2-72.5-78.6-108.3C406.8 109.6 353.2 80 288 80zM95.4 112.6C142.5 68.8 207.2 32 288 32s145.5 36.8 192.6 80.6c46.8 43.5 78.1 95.4 93 131.1c3.3 7.9 3.3 16.7 0 24.6c-14.9 35.7-46.2 87.7-93 131.1C433.5 443.2 368.8 480 288 480s-145.5-36.8-192.6-80.6C48.6 356 17.3 304 2.5 268.3c-3.3-7.9-3.3-16.7 0-24.6C17.3 208 48.6 156 95.4 112.6zM288 336c44.2 0 80-35.8 80-80s-35.8-80-80-80c-.7 0-1.3 0-2 0c1.3 5.1 2 10.5 2 16c0 35.3-28.7 64-64 64c-5.5 0-10.9-.7-16-2c0 .7 0 1.3 0 2c0 44.2 35.8 80 80 80zm0-208a128 128 0 1 1 0 256 128 128 0 1 1 0-256z"/></svg>
                                     </button>
                                 </div>
@@ -78,7 +72,7 @@
                         </div>
                     </div>
                     <div class="w-full flex items-center">
-                        <button name="continue" class="bg-[#ffc8a8] px-10 py-2 rounded-xl">Continue</button>
+                        <button name="signup" class="bg-[#ffc8a8] px-10 py-2 rounded-xl">Continue</button>
                     </div>
                 </form>
             </div>
